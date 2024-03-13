20fe25531a8539c6e654282702b1f182620e54ca
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-13 15:50:20 +0100

    feat(home): update home page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Header } from "./components/heading/header";
+import { SettingsButton } from "./components/settings/settings-button";
+import { Timer } from "./components/timer/timer";
 import { cn } from "./utils";
 
 function App() {
   return (
     <main
       className={cn(
-        "flex justify-between flex-col items-center w-full h-full px-6 pt-8 pb-12 bg-dark-blue text-light-blue",
-        "font-space-mono"
+        "flex flex-col items-center w-full h-full gap-12 px-6 pt-8 pb-12 bg-dark-blue text-light-blue min-h-screen",
+        "font-kumbh-sans"
       )}
     >
       <Header />
-      <section>TIMER</section>
-      <section>SETTINGS</section>
+      <Timer />
+      <SettingsButton />
     </main>
   );
 }
