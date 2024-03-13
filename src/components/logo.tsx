aab62043279b710c20dbb875ed420389e69454e2
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-13 14:53:32 +0100

    feat(logo): create logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,11 @@
+export const Logo = () => {
+  return (
+    <div className="grid place-items-center">
+      <img
+        className="w-[117px] h-6 sm:w-[157px] sm:h-10"
+        src="/logo.svg"
+        alt="logo-with-app-name"
+      />
+    </div>
+  );
+};
