680b856d7891f35ee64f5d32df788aa100525989
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-13 14:53:22 +0100

    feat(button): create button component

---
+++ src/components/button.tsx
@@ -0,0 +1,23 @@
+import { cn } from "../utils";
+
+interface IProps {
+  type: "submit" | "button";
+  onClick: () => void;
+  className?: string;
+  children: React.ReactNode;
+}
+
+export const PomodoroButton: React.FC<IProps> = ({
+  children,
+  type,
+  className,
+}) => {
+  return (
+    <button
+      type={type}
+      className={cn("py-4 font-bold text-base text-white", className)}
+    >
+      {children}
+    </button>
+  );
+};
