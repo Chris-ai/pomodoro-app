977197c988074401f16f09d4ac05aad69693832a
chris-ai <krzysiek.kondracikowski@gmail.com>
2024-03-13 15:50:50 +0100

    feat(settings): add settings button

---
+++ src/components/settings/settings-button.tsx
@@ -0,0 +1,9 @@
+import { SettingsIcon } from "../icons/settings";
+
+export const SettingsButton = () => {
+  return (
+    <button className="bg-transparent cursor-pointer grid place-items-center">
+      <SettingsIcon />
+    </button>
+  );
+};
